dba5b24b8ec44141f35e2dd39483c0ac6888d409
Darren Kulp <darren@kulp.ch>
2022-11-25 19:56:28 -0500

    Use grayscale only

--- make-menu.sh
+++ make-menu.sh
@@ -11,13 +11,9 @@ remove_last_newline ()
 }
 
 spacing=10
-active_color=lightgreen
-for color in white $active_color
-do
-    (IFS=$'\n'; echo "${filenames[*]}") |
-        remove_last_newline |
-        # Without a `-trim`, interlinear spacing of 10 seems to work, but that is probably at best a happy accident.
-        # For `png:color-type=3` see PNG color depth: https://www.w3.org/TR/png/#11IHDR
-        magick -background black -fill transparent -interline-spacing $spacing -font Helvetica -size x460 label:@- -resize '700x>' \
-            -background $color -flatten -repage 720x480 -repage +'%[fx:(%[W]-%[w])/2]+%[fx:(%[H]-%[h])/2]' -define png:color-type=3 -colors 4 -type Palette +write png8:all-$color.png -crop 1x10@ tile-$color-%d.png
-done
+(IFS=$'\n'; echo "${filenames[*]}") |
+    remove_last_newline |
+    # Without a `-trim`, interlinear spacing of 10 seems to work, but that is probably at best a happy accident.
+    # For `png:color-type=3` see PNG color depth: https://www.w3.org/TR/png/#11IHDR
+    magick -background black -fill white -interline-spacing $spacing -font Helvetica -size 'x460>' label:@- -resize '720x>' \
+        -repage 720x480 -repage +'%[fx:(%[W]-%[w])/2]+%[fx:(%[H]-%[h])/2]' -type Grayscale -colors 4 +write mpr:all \( mpr:all -crop 1x10@ +write tile-%d.png \) \( mpr:all -gravity center -extent 720x480 +write all.png -negate +write all-invert.png \) null:
