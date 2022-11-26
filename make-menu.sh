d8edd44a8435b2626878566957bbe42fda7372da
Darren Kulp <darren@kulp.ch>
2022-11-25 21:24:04 -0500

    Crop by correct number of files

--- make-menu.sh
+++ make-menu.sh
@@ -18,7 +18,7 @@ basename -a "${filenames[@]%.*}" |
     # Without a `-trim`, interlinear spacing of 10 seems to work, but that is probably at best a happy accident.
     # For `png:color-type=3` see PNG color depth: https://www.w3.org/TR/png/#11IHDR
     magick -background black -fill white -interline-spacing $spacing -font Helvetica -size 'x460>' label:@- -resize '720x>' \
-        -repage 720x480 -repage +'%[fx:(%[W]-%[w])/2]+%[fx:(%[H]-%[h])/2]' -type Grayscale -colors 3 +write mpr:all \( mpr:all -crop 1x10@ +write tile-%d.png \) \( mpr:all -gravity center -extent 720x480 +write all.png -negate +write all-invert.png \) null:
+        -repage 720x480 -repage +'%[fx:(%[W]-%[w])/2]+%[fx:(%[H]-%[h])/2]' -type Grayscale -colors 3 +write mpr:all \( mpr:all -crop "1x${#filenames[@]}@" +write tile-%d.png \) \( mpr:all -gravity center -extent 720x480 +write all.png -negate +write all-invert.png \) null:
 
 cat <<EOF | xmllint --pretty 1 - > menu.xml
 <subpictures>
